483310b40172dacdbc9739faf8a2f2ab7b02dbde
chrischavarria <chrischavarria2@gmail.com>
2026-05-14 11:00:35 -0700

    Set appScriptUrl for Google Apps Script
    Updated appScriptUrl with a new endpoint.

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.PACKING_ROOM_CONFIG = {
-  appScriptUrl: "",
-  slackWebhookUrl: "",
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbxkpm_rnWa5nYKxwFpUtOBmwnaF0gUExOhCRQRmgFWwmaqHGeEhbj8sZUROUy8BggRU/exec",
   reminderCutoff: "17:15",
   timezoneLabel: "America/Phoenix"
 };
